afe7ebd6f473e26d24a2702a517cb715a8685cd5
vasy <solomon202.pi@yandex.ru>
2021-09-17 02:21:10 +0300

    first commit

---
+++ src/main/java/net/proselyte/springsecuritydemo/model/User.java
@@ -0,0 +1,27 @@
+package net.proselyte.springsecuritydemo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+}
