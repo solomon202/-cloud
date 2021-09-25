818536e58280576948c1c83dfe5131cfe0d8a602
vasy <solomon202.pi@yandex.ru>
2021-09-25 17:59:59 +0300

    ty

--- src/main/java/net/proselyte/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/net/proselyte/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -1,5 +1,5 @@
 package net.proselyte.springsecuritydemo;
-
+//запуск приложения и переходим в браузер по ссылке
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
